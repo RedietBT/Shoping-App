a67bde4fb0a72757f464e777a4f5a04eab86132d
unknown <redietberhanu2@gmail.com>
2025-07-30 12:25:24 +0300

    ConsumerStatfull Widigt lesson done

--- flutter-riverpod-tutorial-starter-project/lib/screens/cart/cart_screen.dart
+++ flutter-riverpod-tutorial-starter-project/lib/screens/cart/cart_screen.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:riverpod_files/providers/products_provider.dart';
 
-class CartScreen extends StatefulWidget {
+class CartScreen extends ConsumerStatefulWidget {
   const CartScreen({super.key});
 
   @override
-  State<CartScreen> createState() => _CartScreenState();
+  ConsumerState<CartScreen> createState() => _CartScreenState();
 }
 
-class _CartScreenState extends State<CartScreen> {
+class _CartScreenState extends ConsumerState<CartScreen> {
   bool showCoupon = true;
 
   @override
   Widget build(BuildContext context) {
+    final cartProducts = ref.watch(reducedProductsProvider);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Cart'),
@@ -20,10 +24,43 @@ class _CartScreenState extends State<CartScreen> {
       ),
       body: Container(
         padding: const EdgeInsets.all(30),
-        child: const Column(
+        child: Column(
           children: [
             Column(
-              children: [], // output cart products here
+              children: cartProducts.map((product) {
+                return Container(
+                  padding: const EdgeInsets.only(top: 10, bottom: 10),
+                  child: Row(
+                    children: [
+                      Image.asset(
+                        product.image,
+                        height: 60,
+                        width: 60,
+                        fit: BoxFit.cover,
+                      ),
+                      const SizedBox(width: 20),
+                      Text('${product.title}...'),
+                      const SizedBox(width: 20),
+                      Text('\$${product.price}'),
+                      const SizedBox(width: 20),
+                      Expanded(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              product.title,
+                              style: const TextStyle(
+                                  fontSize: 16, fontWeight: FontWeight.bold),
+                            ),
+                            const SizedBox(height: 5),
+                            Text('\$${product.price}'),
+                          ],
+                        ),
+                      ),
+                    ],
+                )
+                );
+              }).toList(), // output cart products here
             ),
 
             // output totals here
